b6adfe6fe702ceee483d5f5283fcb293e6a4db8d
Thomas Armena <thomasarmena@gmail.com>
2021-06-12 18:27:21 -0400

    cleaned up some code

--- Scenes/Players/Player/Player.gd
+++ Scenes/Players/Player/Player.gd
@@ -9,7 +9,6 @@ export var shoot_action := "p1_shoot"
 export (NodePath) var target_player_path
 
 var speed = 500
-var velocity = Vector2()
 var BULLET = preload("res://Scenes/Bullet/Bullet.tscn")
 var target_player
 
@@ -19,19 +18,19 @@ func _ready():
 	var circle = get_node("Circle")
 	circle.color = color
 	target_player = get_node(target_player_path)
-
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed(move_right_action):	velocity.x += 1
-	if Input.is_action_pressed(move_left_action): 	velocity.x -= 1
-	if Input.is_action_pressed(move_down_action): 	velocity.y += 1
-	if Input.is_action_pressed(move_up_action):		velocity.y -= 1
-	velocity = velocity.normalized()
-
-func _physics_process(delta):
-	get_input()
-	entity.impulse(velocity+position, speed)
 	
+func _physics_process(delta):
+	move()
+	
+func move():
+	var move_vector = Vector2()
+	if Input.is_action_pressed(move_right_action):	move_vector.x += 1
+	if Input.is_action_pressed(move_left_action): 	move_vector.x -= 1
+	if Input.is_action_pressed(move_down_action): 	move_vector.y += 1
+	if Input.is_action_pressed(move_up_action):		move_vector.y -= 1
+	move_vector = move_vector.normalized()
+	entity.impulse(move_vector+position, speed)
+
 func _process(delta):
 	if Input.is_action_just_pressed(shoot_action):
 		shoot()
