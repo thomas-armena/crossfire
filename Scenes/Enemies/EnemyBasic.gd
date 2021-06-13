3592274646f18f1e48a37ee2f0a25c23993ca705
Michael Le <michael.le647@gmail.com>
2021-06-12 21:56:12 -0400

    Add random moving enemy

--- Scenes/Enemies/EnemyBasic.gd
+++ Scenes/Enemies/EnemyBasic.gd
@@ -1,59 +1,29 @@
 extends KinematicBody2D
 
 onready var entity = get_node("Entity")
+onready var view_width = get_viewport_rect().size.x
+onready var view_height = get_viewport_rect().size.y
 
-enum EnemyState {
-	CHASE,
-	EVADE,
-}
-var state = EnemyState.CHASE
-
-var acceleration = Vector2(0, 9)
-var velocity = Vector2(2, 0)
-
-var damping = Vector2(0.8, 0.8)
-var friction = Vector2(0.8, 0.8)
+var rng = RandomNumberGenerator.new()
+var target = Vector2()
+export (float) var speed = 300 
 
 func _ready():
-	print(entity.hp)
+	rng.randomize()
+	target = Vector2(rng.randf_range(0, view_width), rng.randf_range(0, view_height))
 
-	
 func update_behaviour(delta):
 	var target_pos = get_global_mouse_position()
-	var distance = target_pos.distance_to(position)
-	
-	if (distance < 32):
-		state = EnemyState.EVADE
-	
-	if (distance > 200):
-		state = EnemyState.CHASE
-	
-	if (state == EnemyState.CHASE):
-		look_at(get_global_mouse_position())
-	else:
-		look_at(-get_global_mouse_position())
+	var distance = target.distance_to(position)
+
+	if (abs(distance) < 32):
+		target = Vector2(rng.randf_range(0, view_width), rng.randf_range(0, view_height))
 		
-	
-func update_movement(delta):
-	pass
-	
-func impulse(target_pos, strength):
-	var direction = target_pos - position
-	print(direction.normalized())
-	acceleration += direction.normalized() * strength
-	
+	entity.impulse(target, speed)
 	
 func _input(event):
 	if (event.is_pressed() and event.button_index == BUTTON_LEFT):
-		impulse(get_global_mouse_position(), 10000)
+		entity.impulse(get_global_mouse_position(), 10000)
 		
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
-	
-	# Update speed and position
-	velocity += acceleration * delta
-	position += velocity * delta
-	
-	# Apply friction and damping
-	acceleration *= damping
-	velocity *= friction
+	update_behaviour(delta)
--- Scenes/Entity.gd
+++ Scenes/Entity.gd
@@ -2,9 +2,21 @@ extends Node2D
 
 export (float) var hp = 50
 export (float) var dmg = 1
-export (float) var movement_speed = 10
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var acceleration = Vector2()
+var velocity = Vector2()
 
+export (Vector2) var damping = Vector2(0.8, 0.8)
+export (Vector2) var friction = Vector2(0.8, 0.8)
+
+func impulse(target_pos, strength):
+	var direction = target_pos - get_parent().position
+	acceleration += direction.normalized() * strength
+
+func _physics_process(delta):
+	velocity += acceleration * delta
+	get_parent().position += velocity * delta
+	get_parent().rotation = velocity.angle()
+	
+	acceleration *= damping
+	velocity *= friction
