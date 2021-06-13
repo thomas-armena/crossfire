518e59475dacbf80645ae71d221e9595af3ef183
Michael Le <michael.le647@gmail.com>
2021-06-12 23:10:39 -0400

    Add new dash enemy

---
+++ Scenes/Enemies/EnemyDash.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+
+export (NodePath) var target_player;
+
+onready var entity = get_node("Entity")
+onready var target = get_node(target_player)
+
+export (float) var speed = 30000 
+export (float) var dash_time = 2
+
+var dash_timer = Timer.new()
+
+func _ready():
+	add_child(dash_timer)
+	dash_timer.connect("timeout", self, "_on_dash_timeout")
+	dash_timer.start(dash_time)
+	
+func _physics_process(delta):
+	if (!target):
+		return
+	look_at(target.position)
+
+func _on_dash_timeout():
+	if (!target):
+		return
+	entity.impulse(target.position, speed)
