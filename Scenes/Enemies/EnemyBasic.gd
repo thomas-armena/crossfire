7b6c73b0948b32ae86bd0807d85d9b5dea32bd5b
Michael Le <michael.le647@gmail.com>
2021-06-12 21:00:19 -0400

    Add enemy movement

--- Scenes/Enemies/EnemyBasic.gd
+++ Scenes/Enemies/EnemyBasic.gd
@@ -2,14 +2,42 @@ extends KinematicBody2D
 
 onready var entity = get_node("Entity")
 
+enum EnemyState {
+	CHASE,
+	EVADE,
+}
+
 var velocity = Vector2()
+var state = EnemyState.CHASE
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(entity.hp)
 
+	
+func update_behaviour(delta):
+	var target_pos = get_global_mouse_position()
+	var distance = target_pos.distance_to(position)
+	
+	if (distance < 32):
+		state = EnemyState.EVADE
+	
+	if (distance > 200):
+		state = EnemyState.CHASE
+	
+	if (state == EnemyState.CHASE):
+		look_at(get_global_mouse_position())
+	else:
+		look_at(-get_global_mouse_position())
+		
+	
+func update_movement(delta):
+	velocity = Vector2(entity.movement_speed, 0).rotated(rotation)
+	velocity = move_and_slide(velocity * delta)
 
 
-
-func _process(delta):
-	pass
+func _physics_process(delta):
+	update_behaviour(delta)
+	update_movement(delta)
+	
