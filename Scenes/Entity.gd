5479a3c805fc772bbd9fba1dbdae4ecf3ea472f7
Michael Le <michael.le647@gmail.com>
2021-06-12 20:01:25 -0400

    Add entity scene

---
+++ Scenes/Entity.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export (int) var hp = 50
+export (int) var dmg = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
