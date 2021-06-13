6eaa5786315a7421107f7bd3728da8c976a77cc1
Michael Le <michael.le647@gmail.com>
2021-06-12 22:36:44 -0400

    Add auto rotate bool to entity

--- Scenes/Enemies/EnemyChase.gd
+++ Scenes/Enemies/EnemyChase.gd
@@ -4,21 +4,25 @@ onready var entity = get_node("Entity")
 onready var view_width = get_viewport_rect().size.x
 onready var view_height = get_viewport_rect().size.y
 
-var rng = RandomNumberGenerator.new()
 var target = Vector2()
-export (float) var speed = 300 
+export (float) var speed = 30000 
+
+var dash_timer = Timer.new()
 
 func _ready():
-	rng.randomize()
-	target = Vector2(rng.randf_range(0, view_width), rng.randf_range(0, view_height))
+	dash_timer.connect("timeout", self, "_on_dash_timeout")
+	add_child(dash_timer)
+	dash_timer.start(1)
+	
 
 func update_behaviour(delta):
 	var distance = target.distance_to(position)
-
-	if (abs(distance) < 32):
-		target = Vector2(rng.randf_range(0, view_width), rng.randf_range(0, view_height))
-		
-	entity.impulse(target, speed)
+	
 
 func _physics_process(delta):
+	look_at(get_global_mouse_position())
 	update_behaviour(delta)
+
+func _on_dash_timeout():
+	print("DASH")
+	entity.impulse(get_global_mouse_position(), speed)
